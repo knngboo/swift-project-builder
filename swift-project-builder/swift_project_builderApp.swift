0d712843a7efca78d76c91ad635857a462bd0c34
orlando <216954379+knngboo@users.noreply.github.com>
2026-04-28 12:54:10 -0500

    Initial Commit

---
+++ swift-project-builder/swift_project_builderApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_project_builderApp.swift
+//  swift-project-builder
+//
+//  Created by King Boo on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_project_builderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
